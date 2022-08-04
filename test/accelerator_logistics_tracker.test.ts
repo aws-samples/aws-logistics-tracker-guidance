ad25b83340122f2b9315a179337a909590dbf1dc
miketran <miketran@amazon.com>
2022-08-04 10:31:41 -0700

    Initial commit

---
+++ test/accelerator_logistics_tracker.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AcceleratorLogisticsTracker from '../lib/accelerator_logistics_tracker-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/accelerator_logistics_tracker-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AcceleratorLogisticsTracker.AcceleratorLogisticsTrackerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
